e55e84f5493c72d651b4284b316f163cbe77dbeb
Rob Holden <robholdenflag@gmail.com>
2017-05-05 00:04:02 +0100

    Events can now be checked against occupied grid squares
    .Created find event method
    .Tested for same grid square event

--- lib/grid_square.rb
+++ lib/grid_square.rb
@@ -1,9 +1,28 @@
 class GridSquare
 
-  attr_reader :coordinates
+  attr_reader :coordinates, :event
 
   def initialize(x,y)
     @coordinates = x,y
+    @event = nil
+    find_event
+    end
+
+    def find_event
+      events = []
+      csv = File.read('event_data.csv')
+      events_list = JSON.parse(csv)
+      for i in 0..(events_list.length)
+        if Event.new(i) != "Event doesn't exist."
+          events.push(Event.new(i))
+          i  += 1
+        end
+      end
+      events.each do |e|
+        if e.coords == @coordinates
+          @event = [e.id, e.no_tickets, e.price, e.coords] if @event == nil
+        end
+      end
   end
-  
+
 end
--- spec/grid_square_spec.rb
+++ spec/grid_square_spec.rb
@@ -1,14 +1,20 @@
 require 'grid_square'
 
 describe GridSquare do
-   subject(:grid_square) { GridSquare.new(3, 8) }
+   let(:grid_square) { double :grid_square, coordinates: [3, 0],  event: nil}
+   subject(:grid_square2) { GridSquare.new(3,0)}
 
-   it 'Is an instance of a grid square' do
-     expect(subject).to be_an_instance_of(GridSquare)
+   it 'Has x and y coordinates' do
+      expect(grid_square.coordinates).to eq([3, 0])
    end
 
-   it 'Has been given both x and y coordinates' do
-      expect(subject.coordinates).to eq([3,8])
-   end
+   it 'Has no existing events' do
+      expect(grid_square.event).to eq(nil)
+    end
 
- end
+describe '::event' do
+   it 'Loads event information if an event in the grid square exists' do
+       expect(grid_square2.event).to eq([1, 37, "$34.48", [3, 0]])
+   end
+end
+end
