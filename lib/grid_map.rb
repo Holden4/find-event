b1de040c099c22e125b61cdd2731d1ab4f63f434
Rob Holden <robholdenflag@gmail.com>
2017-05-04 18:12:48 +0100

    Create grid squares with a new instance of grid map
    .Deleted reduntant test
    .441 is the value of both axis added together 21 x 21

--- lib/grid_map.rb
+++ lib/grid_map.rb
@@ -5,4 +5,10 @@ class GridMap
   def initialize
     @grid_squares = []
   end
+
+  def generate_grid_squares
+    441.times do
+      @grid_squares.push("a grid square")
+    end
+  end
 end
--- spec/grid_map_spec.rb
+++ spec/grid_map_spec.rb
@@ -1,16 +1,11 @@
 require 'grid_map'
 
- describe GridMap do
-   subject(:grid_map) { GridMap.new }
+  describe GridMap do
+    subject(:grid_map) { GridMap.new }
 
-   describe 'Grid is initialized' do
-     it 'Creates a new grid' do
-       expect(subject).to be_an_instance_of(GridMap)
-     end
+    it 'Creates a grid map containing all grid squares' do
+      subject.generate_grid_squares
+      expect(subject.grid_squares.count).to eq(441)
 
-
-      it 'contains grid squares' do
-        expect(subject.grid_squares).to eq([])
-      end
     end
   end
