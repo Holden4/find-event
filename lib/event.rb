77ae90bbab83b4818c24b6ab11dde12ffd3012a4
Rob Holden <robholdenflag@gmail.com>
2017-05-04 23:36:53 +0100

    Created relevant attributes for events
    .id
    .no_tickets
    .price
    .coords

--- lib/event.rb
+++ lib/event.rb
@@ -1,15 +1,26 @@
 require 'csv'
 
  class Event
-   attr_reader :id
+   attr_reader :id, :no_tickets, :price, :coords
 
    def initialize(id)
      @id = id
+     @no_tickets = nil
+     @price = nil
+     @coords = nil
+     events_catalog
    end
 
    def events_catalog
      csv = File.read('event_data.csv')
      events = JSON.parse(csv)
+     events.each do |event|
+       if event["id"] == @id
+         @no_tickets = event["no_tickets"]
+         @price = event["price"]
+         @coords = event["x_coord"], event["y_coord"]
+       end
+     end
    end
 
  end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -3,8 +3,21 @@ require 'event'
  describe Event do
    subject(:event) { Event.new(1) }
 
-   it 'Creates new event with id' do
-     expect(subject.id).to eq(1)
-   end
+   describe 'attributes' do
+     it 'Has a unique id' do
+       expect(subject.id).to eq(1)
+     end
 
+     it 'Has a certain number of tickets' do
+       expect(subject.no_tickets).to eq(37)
+     end
+
+     it 'Has a price in USD' do
+       expect(subject.price).to eq("$34.48")
+     end
+
+     it 'Has a set of coordinates' do
+       expect(subject.coords).to eq([3, 0])
+     end
+   end
  end
