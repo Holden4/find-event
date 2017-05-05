9e67314faaaac6568333397c3b749a92c752f44f
Rob Holden <robholdenflag@gmail.com>
2017-05-05 15:59:20 +0100

    Method for identifying closest events re added

--- lib/grid_map.rb
+++ lib/grid_map.rb
@@ -27,4 +27,24 @@ private
     end
   end
 
+  def find_closest_events(coords)
+    counter = 0
+    hash = {}
+    output = "Closest Events to (#{coords})\n"
+
+    @event_squares.each do |grid_squares|
+      xdifference = (coords.split(",")[0].to_i) - (grid_squares[0].split(",")[0].to_i)
+      ydifference = (coords.split(",")[1].to_i) - (grid_squares[0].split(",")[1].to_i)
+      distance = xdifference.abs + ydifference.abs
+      hash.store(distance, grid_squares[0])
+    end
+    hash.sort_by{|k,v| k}.to_h.each do |k, v|
+        c = @grid_squares[v]
+        output += "Event #{sprintf '%03d', c.event[:id]} -  #{c.event[:price]}, Distance #{k}" + "\n"
+        counter += 1
+        break if counter == 5
+    end
+    puts output
+  end
+
 end
